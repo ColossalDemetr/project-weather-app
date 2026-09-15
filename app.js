580451ddc781b4a616258a966f82bad3fbb980eb
dev-colossal <241526608+ColossalDemetr@users.noreply.github.com>
2026-09-15 22:45:08 +0300

    Add searchWeather function and search for desirable location

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const API_KEY = '8EJFXK7L684D5QFP6EGAZUKCL';
 const test = document.getElementById('call');
+const search = document.getElementById('search');
 
 
 
@@ -16,6 +17,21 @@ async function fetchWeather () {
     }
 }; 
 
+
+async function searchWeather () {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}?unitGroup=us&key=${API_KEY}`);
+        const data = await response.json();
+        console.log({
+            'Temperature': data.currentConditions.temp, 
+            'Desc': data.description, 
+            'Location': data.resolvedAddress});
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 test.addEventListener('click', () => {
-    fetchWeather();
+    searchWeather();
 });
