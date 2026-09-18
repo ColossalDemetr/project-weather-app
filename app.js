9acd512ced3c960202e2ba07acef5d7cfb9b5191
dev-colossal <241526608+ColossalDemetr@users.noreply.github.com>
2026-09-18 11:50:41 +0300

    Added some styling to the project

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 const API_KEY = '8EJFXK7L684D5QFP6EGAZUKCL';
 const test = document.getElementById('call');
 const search = document.getElementById('search');
+const mainDiv = document.querySelector('.main-content');
+let isCelsius = false;
+let currentTemp = null;
 
 
+const converToCelsius = (temp) => {
+    const first = temp - 32;
+    const second = first * 5 / 9;
+    
+    return second;
+};
 
-async function fetchWeather () {
-    try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London%2CUK?unitGroup=us&key=${API_KEY}`);
-        const data = await response.json();
-        console.log({
-            'Temperature': data.currentConditions.temp, 
-            'Desc': data.description, 
-            'Location': data.resolvedAddress});
-    } catch (e) {
-        console.log(e);
-    }
-}; 
+const convertToFahrenheit = (temp) => {
+    const first = temp * 1.8;
+    const second = first + 32;
 
+    return second;
+};
 
 async function searchWeather () {
+    if (!search.value) return;
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}?unitGroup=us&key=${API_KEY}`);
         const data = await response.json();
-        console.log({
-            'Temperature': data.currentConditions.temp, 
-            'Desc': data.description, 
-            'Location': data.resolvedAddress});
-        console.log(data);
+
+        currentTemp = data.currentConditions.temp;
+
+
+        mainDiv.innerHTML = `
+            <h3 class="address">${data.resolvedAddress}</h3>
+            <p class="description">${data.description}</p>
+            <p class="temperature">${data.currentConditions.temp} ℉</p>
+            <button id="changeUnitGroup">View in Celsius</button>
+        `
+
+        document.getElementById('changeUnitGroup').addEventListener('click', () => {
+        isCelsius = !isCelsius;
+
+        const temp = isCelsius ? converToCelsius(currentTemp) : currentTemp;
+        const unit = isCelsius ? '℃' : '℉';
+
+        document.querySelector('.temperature').textContent = `${temp.toFixed(1)}${unit}`;
+        document.getElementById('changeUnitGroup').textContent = isCelsius ? 'View in Fahrenheit' : 'View in Celsius';
+});
     } catch (error) {
         console.log(error);
     }
@@ -35,3 +53,4 @@ async function searchWeather () {
 test.addEventListener('click', () => {
     searchWeather();
 });
+
