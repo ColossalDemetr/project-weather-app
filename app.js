39f81c0771787154a45723ceec1fa07ba8ce6f4b
dev-colossal <241526608+ColossalDemetr@users.noreply.github.com>
2026-09-18 12:45:47 +0300

    Add error messages and styling to main content

--- app.js
+++ app.js
@@ -22,23 +22,36 @@ const convertToFahrenheit = (temp) => {
 };
 
 async function searchWeather () {
+    mainDiv.innerHTML = `<p class="addressError">Loading...</p>`
+
     if (!search.value) {
         mainDiv.innerHTML = `
-            <h3 class="address">Type in city/country first!</h3>
+            <h3 class="addressError">Type in city/country first!</h3>
         `
+        return;
     };
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}?unitGroup=us&key=${API_KEY}`);
+        if (!response.ok) {
+            mainDiv.innerHTML = `
+                <h3 class="addressError">Wasn't able to find this one :( Type in another one?</h3>
+            `
+            return;
+        };
+
         const data = await response.json();
 
         currentTemp = data.currentConditions.temp;
 
 
         mainDiv.innerHTML = `
-            <h3 class="address">${data.resolvedAddress}</h3>
-            <p class="description">${data.description}</p>
-            <p class="temperature">${data.currentConditions.temp} ℉</p>
-            <button id="changeUnitGroup">View in Celsius</button>
+            <div> <h3 class="address">${data.resolvedAddress}</h3> </div>
+            
+            <div>
+                <p class="description">${data.description}</p>
+                <p class="temperature">${data.currentConditions.temp}℉</p>
+                <button id="changeUnitGroup">View in Celcius</button>
+            </div>
         `
 
         document.getElementById('changeUnitGroup').addEventListener('click', () => {
